d693c3e91e1d5d204f6571adf81c766fa6b592f2
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2020-08-14 10:45:25 +0300

    First Pages

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Home from './Pages/Home.js';
+import Products from './Pages/Products.js';
+import Contact from './Pages/Contact.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    menuDrop: {
+      marginTop: theme.spacing(6),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    link: {
+      textDecoration: 'none',
+    }
+  }),
+);
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <MenuIcon fontSize="large" style={{color: '#00ff00'}} className={classes.menuButton} />
+          </Button>
+          <Menu id="simple-menu" className={classes.menuDrop} anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+            <MenuItem onClick={handleClose}><Link className={classes.link} to="/">Home</Link></MenuItem>
+            <MenuItem onClick={handleClose}><Link className={classes.link} to="/products">Products</Link></MenuItem>
+            <MenuItem onClick={handleClose}><Link className={classes.link} to="/contact">Contact</Link></MenuItem>
+          </Menu>
+          <Typography variant="h6" className={classes.title}>
+            Charge Spot
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
