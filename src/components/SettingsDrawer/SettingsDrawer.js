90245256c0aabaf5fa7584f9d6f367e5cfaa4426
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2020-09-06 23:02:36 +0300

    More Settings added just for display

--- src/components/SettingsDrawer/SettingsDrawer.js
+++ src/components/SettingsDrawer/SettingsDrawer.js
@@ -6,21 +6,36 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import BackspaceIcon from '@material-ui/icons/Backspace';
 import Button from '@material-ui/core/Button';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+import Typography from '@material-ui/core/Typography';
 
 const Settings = (props) => {
 
     return(
     <Drawer variant="persistent" anchor="right" open={props.open}>
         <List>
-          <ListItem>
-            <Button onClick={props.close} startIcon={<ArrowBackIcon/>} color='primary' variant='contained'> Close Settings</Button>
-          </ListItem>
+            <ListSubheader component="div" id="nested-list-subheader">
+            <Button onClick={props.close} startIcon={<BackspaceIcon/>}></Button>
+                Settings
+            </ListSubheader>
           <Divider/>
           <ListItem>
             <Switch onClick={props.darkMode}></Switch>
             <ListItemText primary="Dark Mode" />
+          </ListItem>
+          <ListItem>
+            <Button startIcon={<TimelineIcon/>}>Usage History</Button>
+          </ListItem>
+          <ListItem>
+            <Button startIcon={<AccountBalanceIcon/>}>Account Balance</Button>
+          </ListItem>
+          <ListItem>
+            <Button startIcon={<CreditCardIcon/>}>Change Credit Card</Button>
           </ListItem>
         </List>
     </Drawer>
