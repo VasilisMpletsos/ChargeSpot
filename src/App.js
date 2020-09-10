8671297684923f6e0aa6b43221071fc4f19c9536
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2020-09-10 14:00:55 +0300

    Fixed Contrast text

--- src/App.js
+++ src/App.js
@@ -20,15 +20,17 @@ const App = () => {
         light: blue[500],
         main: blue[700],
         dark: blue[900],
+        contrastText: "#ffffff",
       },
       secondary: {
         light: lime[200],
         main: lime[300],
         dark: lime[400],
+        contrastText: "#ffffff",
       },
       // Used by `getContrastText()` to maximize the contrast between
       // the background and the text.
-      contrastThreshold: 1,
+      contrastThreshold: 5,
       // Used by the functions below to shift a color's luminance by approximately
       // two indexes within its tonal palette.
       tonalOffset: 0.5,
