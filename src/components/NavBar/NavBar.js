1255892381d3f4ded09dab600c99e8eaf6eb0906
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2020-10-03 20:02:08 +0300

    Added ui username on navbar if authenticated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -25,6 +25,34 @@ const NavBar = (props) => {
     setOpen(false);
   };
 
+  // Here we show the username if the user is authenticated,
+  // or we show the Signup & Login options!
+
+  // I have to implement this with redux in order to have global username,
+  // authenticated and many more variables
+
+  const [auth, setAuth] = React.useState(false);
+
+  let show;
+  if (auth) {
+    show = <div className={[classes.showNav, classes.navName].join(" ")}>Vasilis Mpletsos</div>;
+  } else {
+    show = (
+      <div className={classes.showNav}>
+        <Button>
+          <NavLink activeClassName={classes.active} to='/login'>
+            Login
+          </NavLink>
+        </Button>
+        <Button>
+          <NavLink activeClassName={classes.active} to='/signup'>
+            Signup
+          </NavLink>
+        </Button>
+      </div>
+    );
+  }
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -37,26 +65,15 @@ const NavBar = (props) => {
     <AppBar
       position='static'
       style={{
-        background:
-          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 67%, rgba(0,212,255,1) 100%)",
+        background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 67%, rgba(0,212,255,1) 100%)",
       }}
       className={classes.NavBar}
     >
       <Toolbar className={classes.NavBar}>
-        <Button
-          aria-controls='simple-menu'
-          aria-haspopup='true'
-          onClick={handleClick}
-        >
+        <Button aria-controls='simple-menu' aria-haspopup='true' onClick={handleClick}>
           <MenuIcon fontSize='large' color='secondary' />
         </Button>
-        <Menu
-          className={classes.Dropdown}
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
+        <Menu className={classes.Dropdown} anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
           <MenuItem onClick={handleClose} style={{ backgroundColor: "white" }}>
             <Link to='/'>Home</Link>
           </MenuItem>
@@ -70,28 +87,14 @@ const NavBar = (props) => {
         <Hidden xsDown={true}>
           <Typography variant='h6'>Charge Spot</Typography>
         </Hidden>
-
         <div className={classes.Logsign}>
-          <Button>
-            <NavLink activeClassName={classes.active} to='/login'>
-              Login
-            </NavLink>
-          </Button>
-          <Button>
-            <NavLink activeClassName={classes.active} to='/signup'>
-              Signup
-            </NavLink>
-          </Button>
+          <Hidden xsDown={true}>{show}</Hidden>
           <Button>
             <SettingsIcon onClick={handleDrawerOpen}></SettingsIcon>
           </Button>
         </div>
       </Toolbar>
-      <Settings
-        open={open}
-        close={handleDrawerClose}
-        darkMode={props.darkMode}
-      />
+      <Settings open={open} close={handleDrawerClose} darkMode={props.darkMode} />
     </AppBar>
   );
 };
